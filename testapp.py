0715baa1a3211bf4bce2f5e47ead6fa420e427a1
Tom <robertto@tcd.ie>
2021-12-19 04:05:12 +0000

    check token & updated charts

--- testapp.py
+++ testapp.py
@@ -1,54 +1,75 @@
-import yfinance as yf
 import streamlit as st
 import streamlit.components.v1 as stc
 from github import Github
-import datetime
-import plotly.express as px
-import pandas as pd
 import plotly.graph_objects as go
 from math import pi
 from collections import Counter
 
-# A simple webapp used to display data, using streamlit.
 
 # getting the Github User Auth Token from token.txt
 file = open("token.txt")
 token = file.read()
 file.close()
 
-# connecting to github api via access token
-g = Github(token)
 
 st.set_page_config(layout="wide")
 
 
+# connecting to github api via access token
+valid_token = True
+g = Github(token)
+try:
+    # running a sample "get_user" call with token
+    testTokenCallback = g.get_user("tomroberts201")
+except Exception as e:
+    # if an error occurs, the entered token is invalid
+    valid_token = False
+
+
+# A function which displays a user's profile information in their respective columns.
+#   Parameter(s):
+#       - Github user object
+#
+#   Displays the following:
+#       - Profile Avatar (in column 1_1)
+#       - Username, Name, Location (in column 1_2)
 def display_user_info(usr):
     userImage = '"' + usr.avatar_url + '"'
     userName = usr.name
     userLogin = usr.login
     userLocation = usr.location
+    userLink = usr.html_url
 
     with col1_1:
-        stc.html("""<body>
-
-        <style>
-        img {
-          border-radius: 50%;
-        }
-        </style>
-        <img src= """ + userImage + """alt ="Avatar" style="width:200px">
-
-        </body>""", width=250, height=250)
+        # html component to display avatar image rounded
+        stc.html("""
+        <body>
+            <style>
+                img {
+                    border-radius: 50%;
+                }
+            </style>
+            <img src= """ + userImage + """alt ="Avatar" style="width:175px">
+        </body>""",
+        width=250, height=250)
     col1_2.markdown("##### " + userLogin)
     if userName is not None:
         col1_2.write(" " + userName)
     if userLocation is not None:
         col1_2.write(userLocation)
+    if userLink is not None:
+        col1_2.markdown("""[Link to Profile](""" + userLink + """)""")
 
 
-def get_repo_data(userRepos):
+# A function which get's the data from all of a user's repositories.
+#   Parameter(s):
+#       - User Repositories
+#
+#   Returns:
+#       - A list of dictionaries containing the relevant information for each repository.
+def get_repo_data(user_repos):
     repos = []
-    for i in userRepos:
+    for i in user_repos:
         dct = {
             "name": i.name,
             "stars": i.stargazers_count,
@@ -58,39 +79,6 @@ def get_repo_data(userRepos):
         repos.append(dct)
     return repos
 
-
-def get_repo_names(userRepos):
-    nameList = []
-    for i in userRepos:
-        nameList.append(i.name)
-    return nameList
-
-
-def get_stars_per_repo(repos):
-    sprList = []
-    for i in repos:
-        dct = {
-            "name": i.get("name"),
-            "stars": i.get("stars")
-        }
-        if i.get("stars") != 0:
-            sprList.append(dct)
-
-    return sprList
-
-
-def get_total_stars(userRepos):
-    total = 0
-    for i in userRepos:
-        total = total + i.stargazers_count
-    return total
-
-
-def get_repo_sizes(userRepos):
-    dct = {}
-    for i in userRepos:
-        dct[i.name] = i.size
-    return dct
 
 
 # -----  SIDEBAR CODE  ----- #
@@ -107,7 +95,7 @@ if "/" not in user_in and len(user_in) > 3:
     input_type = "user"
     try:
         usr = g.get_user(user_in)
-    except:
+    except Exception as e:
         st.sidebar.write("User not found")
         valid_input = False
 
@@ -120,7 +108,7 @@ if "/" in user_in and len(user_in) > 5:
         user_in = user_in[11:]
     try:
         rpo = g.get_repo(user_in)
-    except:
+    except Exception as e:
         st.sidebar.write("Repository not found")
         valid_input = False
 
@@ -140,17 +128,24 @@ if valid_input and input_type == "user":
 
 
 # -----  TOP BAR  ----- #
-if input_type == "user":
-    st.title('👥 User Data Visualisation')
-    st.write("")
-if input_type == "repo":
-    st.title('👥 Repository Data Visualisation')
-if input_type == "null":
-    st.title('👥 GitHub API Data Visualisation')
-    st.write("Enter a username, or link a repository to begin! :)")
+# Displaying the various titles based on what "page" we're on.
+if valid_token == False:
+    st.title('⚠ Invalid Auth Token')
+    st.write("Please make sure that a valid Auth token has been provided in the \"token.txt\" file.")
 
-col1_1, col1_2, col1_3, col1_4, col1_5 = st.columns([1.0, 1.5, 1, 1, 2])
-# st.write("Current Repository: " + cRepo)
+if input_type == "user":
+    st.title('👥 User Dashboard')
+    st.write("")
+    col1_1, col1_2, col1_3, col1_4, col1_5 = st.columns([1.0, 1.5, 1, 1, 2])
+
+if input_type == "repo":
+    st.title('📁 Repository Data Visualisation')
+
+if input_type == "null" and valid_token == True:
+    st.title('📈 GitHub API Data Visualisation')
+    st.write("Enter a username or repository link to begin! : )")
+
+
 # st.caption("Link to the repository [here](https://github.com/" + lRepo +")")
 
 
@@ -159,34 +154,16 @@ col1_1, col1_2, col1_3, col1_4, col1_5 = st.columns([1.0, 1.5, 1, 1, 2])
 #   +   Also initialises the variables used by other columns
 if valid_input and input_type == "user":
     display_user_info(usr)
-    repoData = get_repo_data(usr.get_repos())
-    st.write(repoData)
-    starsPerRepo = get_stars_per_repo(repoData)
-    st.write(starsPerRepo)
-
+    userRepoData = get_repo_data(usr.get_repos())
 if valid_input and input_type == "repo":
     st.write(rpo.name)
 
 
-# currentRepo = g.get_repo(lRepo)
-# ownerImage = currentRepo.owner.avatar_url
-# ownerName = currentRepo.owner.name
-# ownerUser = currentRepo.owner.login
-
-
-# col1_1.markdown("![Avatar of Repo Owner ><]("+ ownerImage + "&s=150)")
-# col1_1.caption("###### Owner of Repository: ")
-# if ownerName is not None:
-#     col1_1.markdown("""##### **""" + ownerName + """**""" + """ ("""+ ownerUser + """)""")
-# else:
-#     col1_1.markdown("""##### **""" + ownerUser + """**""")
-
-
-# Column 1_3:
+# -----  Column 1_3  ----- #
 #   +   Display Followers & Total Repos
 if valid_input and input_type == "user":
     col1_3.metric("Followers", usr.followers)
-    col1_3.metric("Public Repos", len(repoData))
+    col1_3.metric("Public Repos", len(userRepoData))
 
 # rnlabels = []
 # rnvalues = []
@@ -223,67 +200,95 @@ if valid_input and input_type == "user":
 if valid_input and input_type == "user":
 
     totalStars = 0
-    for i in repoData:
+    for i in userRepoData:
         totalStars += i.get("stars")
 
     col1_4.metric("Following", usr.following)
     col1_4.metric("Stars Received", totalStars)
 
-# langdict = get_repo_languages(cRepo)
-
-# # getting the usernames and values from the languages
-# plabels = []
-# pvalues = []
-
-# for key in langdict.keys():
-#     plabels.append(key)
-# for val in langdict.values():
-#     pvalues.append(val)
-# fig = go.Figure(data=[go.Pie(labels=plabels, values=pvalues)])
-# fig.update_layout(title='Repository Languages:', autosize=False,
-#                     width=600, height=400)
-# if len(plabels) != 0:
-#     col1_3.plotly_chart(fig, use_container_width=True)
-
 
 # ----- COLUMN 2-1 ----- #
 
-col2_1, col2_2, col2_3 = st.columns(3)
-
 if valid_input and input_type == "user":
-    # Displays PieChart of user's main repo languages
+    col2_1, col2_2, col2_3 = st.columns(3)
+
+# Displays PieChart of user's main repo languages
+if valid_input and input_type == "user":
     languages = []
 
-    for i in repoData:
+    for i in userRepoData:
         languages.append(i.get("language"))
 
+    # dictionary of "language":count
     clanguages = dict(Counter(languages))
-    plabels = list(clanguages.keys())
-    pvalues = list(clanguages.values())
 
-    fig = go.Figure(data=[go.Pie(labels=plabels, values=pvalues)])
-    fig.update_layout(title='Repository Languages:', autosize=False,
-                      width=650, height=500)
-    if len(plabels) != 0:
-        col2_1.plotly_chart(fig, use_container_width=True)
+    sort_orders = (sorted(clanguages.items(), key=lambda x:x[1],reverse=True))
+
+    if len(sort_orders) > 10:
+        clanguages = dict(sort_orders[:10])
+
+        plabels = list(clanguages.keys())
+        pvalues = list(clanguages.values())
+
+        fig = go.Figure(data=[go.Pie(labels=plabels, values=pvalues)])
+        fig.update_layout(title='Repository Languages (Top 10):', autosize=False,
+                        width=650, height=500)
+        if len(plabels) != 0:
+            col2_1.plotly_chart(fig, use_container_width=True)
+    else:
+        clanguages = dict(sort_orders)
+
+        plabels = list(clanguages.keys())
+        pvalues = list(clanguages.values())
+
+        fig = go.Figure(data=[go.Pie(labels=plabels, values=pvalues)])
+        fig.update_layout(title='Repository Languages:', autosize=False,
+                        width=650, height=500)
+        if len(plabels) != 0:
+            col2_1.plotly_chart(fig, use_container_width=True)
+
 
 
 # ----- COLUMN 2-2 ----- #
 if valid_input and input_type == "user":
     labels = []
     values = []
+    stars_per_repo = {}
 
-    for i in repoData:
+    for i in userRepoData:
         if i.get("stars") != 0:
-            labels.append(i.get("name"))
-            values.append(i.get("stars"))
+            # adding value to dictionary
+            stars_per_repo[i.get("name")]= i.get("stars")
 
-    fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
-    fig.update_layout(title='Stars per Repo:', autosize=False,
-                      width=650, height=500)
-    fig.update_traces(hoverinfo='label+value', textinfo='value')
-    if len(labels) != 0:
-        col2_2.plotly_chart(fig, use_container_width=True)
+    st.write(stars_per_repo)
+    sort_orders = (sorted(stars_per_repo.items(), key=lambda x:x[1],reverse=True))
+
+
+    if len(sort_orders) > 10:
+        stars_per_repo = dict(sort_orders[:10])
+
+        labels = list(stars_per_repo.keys())
+        values = list(stars_per_repo.values())
+        fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
+        fig.update_layout(title='Stars per Repo (Top 10):', autosize=False,
+                        width=650, height=500)
+        fig.update_traces(hoverinfo='label+value', textinfo='value')
+        if len(labels) != 0:
+            col2_2.plotly_chart(fig, use_container_width=True)
+    else:
+        stars_per_repo = dict(sort_orders)
+
+        labels = list(stars_per_repo.keys())
+        values = list(stars_per_repo.values())
+        fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
+        fig.update_layout(title='Stars per Repo:', autosize=False,
+                        width=650, height=500)
+        fig.update_traces(hoverinfo='label+value', textinfo='value')
+        if len(labels) != 0:
+            col2_2.plotly_chart(fig, use_container_width=True)
+
+
+
 
 
 # ----- COLUMN 2-3 ----- #
@@ -291,7 +296,7 @@ if valid_input and input_type == "user":
     names = []
     stars = []
 
-    for i in repoData:
+    for i in userRepoData:
         names.append(i.get("name"))
         stars.append(i.get("stars"))
 
